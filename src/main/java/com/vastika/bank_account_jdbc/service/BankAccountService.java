48351b906c63588233e7e211d0c284955b72b64e
bthapa87 <67353457+bthapa87@users.noreply.github.com>
2020-08-10 14:51:01 -0700

    Update BankAccountService.java

--- src/main/java/com/vastika/bank_account_jdbc/service/BankAccountService.java
+++ src/main/java/com/vastika/bank_account_jdbc/service/BankAccountService.java
@@ -1,5 +1,16 @@
 package com.vastika.bank_account_jdbc.service;
 
+import com.vastika.bank_account_jdbc.model.BankAccountBalance;
+import com.vastika.bank_account_jdbc.model.BankAccountInfo;
+
 public interface BankAccountService {
+	
+	int creatNewAccount (BankAccountInfo accountInfo);
+	int saveAccountInfo(BankAccountInfo accountInfo);
+	int updateAccountInfo(BankAccountInfo accountInfo);
+	void DeleteAccountInfo(String unique_id_type);
+	
+	int SaveAccountBalance(BankAccountBalance accountBalance);
+	int UpdateAccountBalance(BankAccountBalance accountBalance);
 
 }
