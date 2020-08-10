2b2ce8f617787cb09e4f1e35d64bda1bddab85fd
bthapa87 <67353457+bthapa87@users.noreply.github.com>
2020-08-10 14:50:04 -0700

    Update BankAccountServiceImpl.java

--- src/main/java/com/vastika/bank_account_jdbc/service/BankAccountServiceImpl.java
+++ src/main/java/com/vastika/bank_account_jdbc/service/BankAccountServiceImpl.java
@@ -1,5 +1,49 @@
 package com.vastika.bank_account_jdbc.service;
 
-public class BankAccountServiceImpl {
+import com.vastika.bank_account_jdbc.dao.BankAccountDao;
+import com.vastika.bank_account_jdbc.dao.BankAccountDaoImpl;
+import com.vastika.bank_account_jdbc.model.BankAccountBalance;
+import com.vastika.bank_account_jdbc.model.BankAccountInfo;
+
+public class BankAccountServiceImpl implements BankAccountService {
+	
+	BankAccountDao bankAccountDao = new BankAccountServiceImpl();
+	
+	
+	public int creatNewAccount(BankAccountInfo accountInfo) {
+		return bankAccountDao.createNewAccount(accountInfo);
+	}
+
+	public int saveAccountInfo(BankAccountInfo accountInfo) {
+		// TODO Auto-generated method stub
+		return bankAccountDao.SaveAccountInfo(accountInfo);
+	}
+
+	public int updateAccountInfo(BankAccountInfo accountInfo) {
+		// TODO Auto-generated method stub
+		return bankAccountDao.updateAccountInfo(accountInfo);
+	}
+
+	public void DeleteAccountInfo(String unique_id_type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int SaveAccountBalance(BankAccountBalance accountBalance) {
+		// TODO Auto-generated method stub
+		return bankAccountDao.SaveAccountBalance(accountBalance);
+	}
+
+	public int UpdateAccountBalance(BankAccountBalance accountBalance) {
+		// TODO Auto-generated method stub
+		return bankAccountDao.UpdateAccountBalance(accountBalance);
+	}
+	
+	
+
+}
+  
+  
+  
 
 }
